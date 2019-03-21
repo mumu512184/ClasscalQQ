c911c9505e2449aa5ddc2881043594f069ecf14a
mumu512184 <2280399982@qq.com>
2019-03-21 09:35:03 +0800

    侧滑框架h文件

---
+++ ClasscalMMQQ/ClasscalMMQQ/classes/Tools/MMSlider-frame/MMSliderViewController.h
@@ -0,0 +1,9 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface MMSliderViewController : UIViewController
+
+- (instancetype)initWithLeftViewController:(UIViewController *)leftVc  withRightViewController:(UIViewController *)rightVc;
+
+@end
